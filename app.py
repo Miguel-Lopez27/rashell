725bf3aced90955719d005dbdeadbe43668f4cbe
Migue <miguel.lopezml.ml28@gmail.com>
2023-05-28 03:48:52 -0600

    Avance

--- app.py
+++ app.py
@@ -2,32 +2,41 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import firestoreCrud as fsCRUD
+import pip
 
+pip.main(["install","openpyxl"])
 # LLamado de datos para firebase
 
-docs = fsCRUD.getDocument()
-
+#docs = fsCRUD.getDocument()
+docs = pd.read_excel('Hack.xlsx')
 
 st.sidebar.header('Barra de navegación')
 
-st.title('Aplicación para el monitorieo de la mina')
+st.title('Estrategia para el monitoreo del reabastecimiento de agua en presa "La Boca" Monterrey')
 
-st.header('La tecnología va de la mano para un trabajador en el conocimiento para prevenirse de los riesgos')
-st.markdown('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat nisl lacinia blandit suscipit. Nullam ac viverra ligula, non blandit augue. Fusce pharetra turpis quam, vel varius ipsum ornare at. Praesent vehicula, orci eget imperdiet pharetra, nisi lacus scelerisque est, quis placerat nisl nisl bibendum nulla. Aliquam lectus eros, porttitor et accumsan id, placerat in leo. Integer eu eros ligula. Mauris et tortor justo. Nam ut cursus quam. Etiam posuere massa vitae blandit congue. Aenean varius sit amet lacus porttitor placerat. Vivamus sit amet orci elementum, molestie felis eu, dictum justo. Fusce maximus ligula in ultrices varius. Vivamus vitae condimentum felis. Praesent ut eros et enim congue venenatis.')
+st.header('Rehidratando Monterrey')
+st.markdown('El escases y falta de la calidad del agua para consumo humano, agrícola, indutrial, etc, ha llegado a ser una de las problemáticas más grandes a nivel mundial, y con muy pocas estrategias costeables para resolverlo agrava su problemática. La humedad relativa que se define como la cantidad de agua contenida en el aire, se ha estado considerando como una muy prometedora y costeable fuente de obtención de agua, y con el respaldo de varios estudios se ha demostrado que puede ser costeable para resolver esta problemática en zonas afectadas.\n')
 
-#Seleccionar una mina
+st.markdown('Monterrey ocupa el segundo lugar de la ciudad más grande de México y demuestra ser además una de las más industrializadas. Sin embargo en el año 2022 enfrentó un problema extremo debido a una falta de abastecimiento de agua por la sequía de una de las principales presas, “La Boca” que da sustento no solo a una ciudad grande como Monterrey sino también a Santiago. Una de las causas fue una demanda excesiva del servicio hídrico, llevando a la suspensión total del mismo.')
+
+
+#st.header('Rehidratando Monterrey')
+#st.markdown('La cantidad de agua dispersa o contenida en el aire, lo que se expresa como humedad relativa, ha sido considerada y estudiada como una fuente prometedora para la obtención de agua, con el objetivo de reabastecer o hacer llegar agua a zonas que no tienen acceso a ella. Siendo así una estrategia para resolver dicho problema.')
+
+st.write(docs)
+#Seleccionar una linea de tuberia
 
 col1, col2 = st.columns(2)
 
 with col1:
    st.header("Historial y sensores")
-   option = st.selectbox('Selecciona una mina: ', [e.to_dict()["nom_mina"] for e in docs])
-   st.dataframe([e.to_dict() for e in docs], use_container_width=True)
+   #option = st.selectbox('Selecciona un sector: ', [e.to_dict()["nom_mina"] for e in docs])
+   #st.dataframe([e.to_dict() for e in docs], use_container_width=True)
 
 with col2:
    st.header("Historial de temperatura y humedad")
-   chart_data = pd.DataFrame(np.random.randn(20, 2),columns=['Temperatura', 'humedad',])
-   st.line_chart(chart_data)
+   #chart_data = pd.DataFrame(np.random.randn(20, 2),columns=['Temperatura', 'humedad',])
+   #st.line_chart(chart_data)
 
 st.header('')
 
--- pages/funcionamiento.py
+++ pages/funcionamiento.py
@@ -6,21 +6,41 @@ from PIL import Image
 st.set_page_config(page_title="Funcionamiento", page_icon="📈")
 st.sidebar.header('Barra de navegación')
 
+st.header('Estrategia')
+st.markdown('Las mallas atrapaniebla son un sistema de tejido plastificado elaborado a partir de polipropileno principalmente. Esta malla tiene la particular propiedad de retener el agua presente en el ambiente a partir de la humedad, niebla, nubes o fuertes vientos. El agua es retenida en forma de gotas muy diminutas que se irán fusionando entre sí para formar gotas mucho más grandes, que después se juntarán para formar un cúmulo de agua. En base a reportes este tipo de mallas pueden llegar a captar agua desde un 20$%$ de humedad relativa como valor mínimo y producir 24 L por m2 de malla.\n')
+
+st.markdown('El principal objetivo es abastecer de suficiente área de esta malla en la zona montañosa que rodea a la Presa la Boca ubicada en Monterrey Nuevo León.')
+
+col0, col0 = st.columns(2)
+with col0:
+    st.header('Datos recolectados')
+    st.markdown('Las mallas atrapaniebla')
+with col0:
+    image0 = Image.open('img/recorrido_drone.jpeg')
+    st.image(image=image0)
+
+
+
+st.header('Funcionamiento')
+st.markdown('El agua suspendida en el aire tiene un tamaño de gota extremadamente pequeño (1-40 µm), esto hace que el aire lo levante y quede flotando. Por un fenómeno conocido como condensación la malla atrapaniebla al chocar con un fuerte viento, neblina o nube, retendría las diminutas gotas de agua haciéndolas que se agrupen entre sí para formar una gota más grande. Esta gota grande tendría un peso mayor que por densidad le permitirá fluir a través de la malla y caer sobre un canal colector conectado a la parte baja de la misma para recibir el agua y mandarla a un contenedor.')
+image = Image.open('img/Diagrama_malla.jpeg')
+st.image(image=image)
+
 st.header('Monitoreo de datos')
 
-st.markdown('Monitoreo de datos del caudal del agua, temperatura, humedad y calidad del aire en el ambiente, ')
+st.markdown('Monitoreo de datos de caudal del agua, temperatura, humedad y calidad del aire en el ambiente, ')
 
 col01, col02 = st.columns(2)
 with col01:
     st.header('Datos recolectados')
-    st.markdown('La malla de rashell, provee ')
+    st.markdown('La malla proove')
 with col02:
-    image0 = Image.open('img/recorrido_drone.jpeg')
-    st.image(image=image0)
+    images = Image.open('img/malla.jpeg')
+    st.image(image=images)
 
-st.header('Demostración de una mina')
-st.markdown('Simulación del dron, haciendo un recorrido dentro de una mina, recolectando datos')
-image1 = Image.open('img/Cave.png')
+st.header('')
+st.markdown('')
+image1 = Image.open('img/rocio.jpeg')
 st.image(image=image1)
     
 
@@ -30,10 +50,10 @@ st.markdown('Colección y envio de datos (Mostrados en la consola de una computa
 col3, col4 = st.columns(2)
 
 with col3:
-    image2 = Image.open('img/monitor_Datos.PNG')
+    image2 = Image.open('img/cumulo.jpeg')
     st.image(image=image2)
 with col4:
-    image02 = Image.open('img/scanMina.jpeg')
+    image02 = Image.open('img/mallas.jpeg')
     st.image(image=image02)
 
 st.header('Control estadistico')
@@ -47,7 +67,7 @@ st.area_chart(chart_data)
 
 st.header('Modelado 3D')
 st.markdown('De acuerdo al recorrido del drone y recolección de imagenes, se creara un modelado 3D para la infromación general de la mina y la capacitación de mineros')
-age3 = Image.open('img/modelado_3d.jpeg')
+age3 = Image.open('img/rocio.jpeg')
 st.image(image=age3)
 
 col1, col2 = st.columns(2)
@@ -57,5 +77,5 @@ with col1:
     st.markdown('Se pleanea hacer un recorrido con una nube de puntos para tener un mejor reconocimiento de gritas y profunidad de la mina')
 
 with col2:
-    age4 = Image.open('img/nube_puntos.jpeg')
+    age4 = Image.open('img/funcion.jpeg')
     st.image(image=age4)
